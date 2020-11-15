2131a829276908f60d0e893f07762c96a6b4a349
Denis Korotin <dkorotin@list.ru>
2020-11-15 21:12:35 +0700

    Компонент Database. Метод Query 3

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 
 require_once 'Database.php';
 
-$users = Database::getInstance()->query("SELECT * FROM users");
+$users = Database::getInstance()->query("SELECT * FROM users WHERE username IN(?, ?)", ['John Doe', 'Jane Koe']);
+
+/*Database::getInstance()->get('users', ['username', '=', 'marlin']);
+Database::getInstance()->delete('users', ['username', '=', 'marlin']);*/
 
 if ($users->error()){
     echo "we have an error";
